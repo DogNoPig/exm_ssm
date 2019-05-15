2d14968a4283cefcb844876608f7bd85d5314f3a
xw <3597906135@qq.com>
2019-05-15 17:01:44 +0800

    controller模块更新

--- exm_ssm_web/src/main/java/com/xw/ssm/controller/OrdersController.java
+++ exm_ssm_web/src/main/java/com/xw/ssm/controller/OrdersController.java
@@ -1,8 +1,10 @@
 package com.xw.ssm.controller;
 
+import com.github.pagehelper.PageInfo;
 import com.xw.ssm.domain.Orders;
 import com.xw.ssm.service.IOrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,20 +16,35 @@ import java.util.List;
  * @author xw
  * @date 2019-05-14 22:05
  */
+@RequestMapping("/orders")
+@Controller
 public class OrdersController {
 
     @Autowired
     private IOrdersService ordersService;
 
     //未分页
-    @RequestMapping("/findAll.do")
+   /* @RequestMapping("/findAll.do")
     public ModelAndView findAll(@RequestParam(name = "page", required = true, defaultValue =
             "1") Integer page, @RequestParam(name = "pageSize", required = true, defaultValue = "10")
                                         Integer pageSize) throws Exception {
         List<Orders> ordersList = ordersService.findAllByPage(page, pageSize);
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("order-list");
+        mv.setViewName("orders-list");
+        System.out.println("ordersList++++"+ordersList.toString());
         mv.addObject("ordersList", ordersList);
+        return mv;
+    }*/
+
+    @RequestMapping("/findAll.do")
+    public ModelAndView findAll(@RequestParam(name = "page", required = true, defaultValue =
+            "1") Integer page, @RequestParam(name = "size", required = true, defaultValue = "10")
+                                        Integer pageSize) throws Exception {
+        List<Orders> ordersList = ordersService.findAllByPage(page, pageSize);
+        PageInfo pageInfo = new PageInfo(ordersList);
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("orders-page-list");
+        mv.addObject("pageInfo", pageInfo);
         return mv;
     }
 
@@ -36,7 +53,8 @@ public class OrdersController {
     public ModelAndView findById(@RequestParam(name = "id", required = true) Integer ordersId) throws Exception{
         ModelAndView mv = new ModelAndView();
         Orders orders = ordersService.findById(ordersId);
-
+        mv.addObject("orders",orders);
+        mv.setViewName("orders-show");
         return mv;
     }
 }
