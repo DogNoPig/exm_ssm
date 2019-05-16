57521226a1bac5d5a8cdc4a4894c03fb7a696c5d
xw <3597906135@qq.com>
2019-05-16 16:00:15 +0800

    20190516 登录相关源码及配置文件

---
+++ exm_ssm_service/src/main/java/com/xw/ssm/service/impl/UserServiceImpl.java
@@ -0,0 +1,61 @@
+package com.xw.ssm.service.impl;
+
+import com.xw.ssm.dao.IUserDao;
+import com.xw.ssm.domain.Role;
+import com.xw.ssm.domain.UserInfo;
+import com.xw.ssm.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author xw
+ * @date 2019/5/16 13:39
+ */
+@Service("userService")
+@Transactional
+public class UserServiceImpl implements IUserService {
+
+    @Autowired
+    private IUserDao userDao;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        UserInfo userInfo = null;
+        try{
+            userInfo = userDao.findByUserName(s);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        //处理自己的用户对象封装成userDetails
+//        User user = new User(userInfo.getUsername(),"{noop}"+userInfo.getPassword(),getAuthority(userInfo.getRoles()));
+        User user = new User(userInfo.getUsername(),"{noop}"+userInfo.getPassword(),userInfo.getStatus() == 0 ? false:true,true,true,true,getAuthority(userInfo.getRoles()));
+        return user;
+    }
+
+
+    /**
+     * 返回一个角色的描述
+     * @return
+     */
+    private List<SimpleGrantedAuthority> getAuthority(List<Role> roles) {
+        List<SimpleGrantedAuthority> list = new ArrayList<>();
+        //循环为用户赋予角色
+        for (Role r:roles) {
+            list.add(new SimpleGrantedAuthority("ROLE_"+r.getRoleName()));
+        }
+
+
+        return list;
+    }
+}
